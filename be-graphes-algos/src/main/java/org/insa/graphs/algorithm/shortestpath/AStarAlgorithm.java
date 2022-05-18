ceed088caf9ea2112b2846d4c58b3c8d43b72a31
Aubery Clea <aubery@insa-20713>
2022-05-18 09:00:07 +0200

    Fastest Astar

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 import org.insa.graphs.algorithm.AbstractSolution;
+import org.insa.graphs.algorithm.AbstractInputData.Mode;
 import org.insa.graphs.algorithm.AbstractSolution.Status;
 import org.insa.graphs.algorithm.utils.BinaryHeap;
 import org.insa.graphs.model.Arc;
@@ -17,21 +18,28 @@ import org.insa.graphs.model.Point;
 
 public class AStarAlgorithm extends DijkstraAlgorithm {
 
-	int destination;
-	Graph graph;
+	ShortestPathData data;
 	
     public AStarAlgorithm(ShortestPathData data) {
         super(data);
-        destination = data.getDestination().getId();
-        graph = data.getGraph();
+        this.data = data;
     }
     
-    
+    @Override
     protected Label createLabel(int i) {
         float infini = Float.MAX_VALUE;
         float cout_estime=0;
+        int destination = data.getDestination().getId();
+    	Graph graph = data.getGraph();
     	if(i!=destination){
-        	cout_estime = (float)Point.distance(graph.get(i).getPoint(), graph.get(destination).getPoint());
+    		if(data.getMode()==Mode.LENGTH) {
+            	cout_estime = (float)Point.distance(graph.get(i).getPoint(), graph.get(destination).getPoint());
+    		} else {
+            	float distance = (float)Point.distance(graph.get(i).getPoint(), graph.get(destination).getPoint());
+            	int speed = graph.getGraphInformation().getMaximumSpeed();
+            	cout_estime=(distance*(float)3.6/speed);
+        		System.out.println(cout_estime);
+    		}
     	}
     	return new LabelStar(i, false, infini, null, cout_estime);
     }
